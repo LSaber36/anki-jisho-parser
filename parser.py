97bb50292c08a6badb941325322f8e1720c98876
LSaber36 <L.Saber36@gmail.com>
2024-12-03 22:14:35 -0800

    Initial Commit

---
+++ parser.py
@@ -0,0 +1,5 @@
+'''
+- This file is meant to get the data from kanji pages on jisho.org and put them in a csv file.
+- This csv file will then have comments and things added to it to make it ready to upload to Anki
+'''
+
