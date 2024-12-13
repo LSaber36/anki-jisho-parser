620b71d8d912f1c80a169c554f494c1ee6bff09a
LSaber36 <L.Saber36@gmail.com>
2024-12-12 20:49:39 -0800

    Added exception handling to project for requests and other exceptions

--- parser.py
+++ parser.py
@@ -73,9 +73,20 @@ def get_kanji_data(kanji, show_data=True, is_radical=False):
 	radical_dict = dict()
 	csv_dict = csv_dict_blank.copy()
 
-	url = f'https://www.jisho.org/search/{kanji}%20%23kanji'
-	response = requests.get(url)
-	html_content = response.content
+	# Always set this first, since it is the unique identifier for the CSV file
+	csv_dict[Headers.Kanji] = kanji
+
+	try:
+		url = f'https://www.jisho.org/search/{kanji}%20%23kanji'
+		response = requests.get(url)
+		html_content = response.content
+	except Exception as e:
+		# If we encounter an exception with getting the response
+		html_content = None
+		print('Encountered Exception When Getting Request:\n{}'.format(traceback.print_exc(e)))
+
+	# Verify that the request was able to successfully get the website data
+	if html_content is not None:
 	soup = BeautifulSoup(html_content, 'html.parser')
 
 
@@ -86,38 +97,38 @@ def get_kanji_data(kanji, show_data=True, is_radical=False):
 
 	yomi_elements = soup.find('div', {'class':'kanji-details__main-readings'})
 
-	kunyomi_elements = yomi_elements.find('dl', {'class':'kun_yomi'})
-	# This allows us to skip this step if there's no Kunyomi, which is common for radicals
-	if kunyomi_elements != None:
-		kunyomi_elements = kunyomi_elements.find_all('dd', {'class':'kanji-details__main-readings-list'})
-		kunyomi_list = [element.get_text().strip() for element in kunyomi_elements]
-		csv_dict[Headers.Kunyomi] = ', '.join(kunyomi_list)
+		kunyomi_elements = yomi_elements.find('dl', {'class':'kun_yomi'})
+		# This allows us to skip this step if there's no Kunyomi, which is common for radicals
+		if kunyomi_elements != None:
+			kunyomi_elements = kunyomi_elements.find_all('dd', {'class':'kanji-details__main-readings-list'})
+			kunyomi_list = [element.get_text().strip() for element in kunyomi_elements]
+			csv_dict[Headers.Kunyomi] = ', '.join(kunyomi_list)
 
-	onyomi_elements = yomi_elements.find('dl', {'class':'on_yomi'})
-	onyomi_elements = onyomi_elements.find_all('dd', {'class':'kanji-details__main-readings-list'})
-	onyomi_list = [element.get_text().strip() for element in onyomi_elements]
-	csv_dict[Headers.Onyomi] = ', '.join(onyomi_list)
-	
-	radical_element = soup.find('div', {'class':'radicals'})
-	radical_element = radical_element.find('span')
-	# Remove the unneeded element so it's easier to get the string you want out of the tree
-	radical_element.find('span', {'class':'radical_meaning'}).extract()
-	csv_dict[Headers.Radical] = radical_element.get_text().strip()
+		onyomi_elements = yomi_elements.find('dl', {'class':'on_yomi'})
+		onyomi_elements = onyomi_elements.find_all('dd', {'class':'kanji-details__main-readings-list'})
+		onyomi_list = [element.get_text().strip() for element in onyomi_elements]
+		csv_dict[Headers.Onyomi] = ', '.join(onyomi_list)
+		
+		radical_element = soup.find('div', {'class':'radicals'})
+		radical_element = radical_element.find('span')
+		# Remove the unneeded element so it's easier to get the string you want out of the tree
+		radical_element.find('span', {'class':'radical_meaning'}).extract()
+		csv_dict[Headers.Radical] = radical_element.get_text().strip()
 
-	if is_radical == False:
-		radical = csv_dict[Headers.Radical]
-		example_data = get_example(kanji)
-		csv_dict[Headers.Example_Word] = example_data[Headers.Example_Word]
-		csv_dict[Headers.Example_Meaning] = example_data[Headers.Example_Meaning]
+		if is_radical == False:
+			radical = csv_dict[Headers.Radical]
+			example_data = get_example(kanji)
+			csv_dict[Headers.Example_Word] = example_data[Headers.Example_Word]
+			csv_dict[Headers.Example_Meaning] = example_data[Headers.Example_Meaning]
 
 		radical_dict = get_data_from_kanji(radical, is_radical=True)
 
-		csv_dict[Headers.Radical_Meaning] = radical_dict[Headers.Meaning]
-		csv_dict[Headers.Radical_Onyomi] = radical_dict[Headers.Onyomi]
-		csv_dict[Headers.Radical_Kunyomi] = radical_dict[Headers.Kunyomi]
+			csv_dict[Headers.Radical_Meaning] = radical_dict[Headers.Meaning]
+			csv_dict[Headers.Radical_Onyomi] = radical_dict[Headers.Onyomi]
+			csv_dict[Headers.Radical_Kunyomi] = radical_dict[Headers.Kunyomi]
 
-		if csv_dict[Headers.Radical_Kunyomi] == '':
-			csv_dict[Headers.Radical_Kunyomi] = 'None'
+			if csv_dict[Headers.Radical_Kunyomi] == '':
+				csv_dict[Headers.Radical_Kunyomi] = 'None'
 
 			if show_data:
 				# Print a bunch of newlines to make the output easier to read
@@ -127,19 +138,34 @@ def get_kanji_data(kanji, show_data=True, is_radical=False):
 				print(f'Kanji: {kanji}')
 				print('CSV Dict:')
 				print(json.dumps(csv_dict, indent=4, ensure_ascii=False))
+	else:
+		csv_dict[Headers.Meaning] = 'Encountered Error'
 
 	return csv_dict
 
 
 if __name__ == '__main__':
-	
-	input_filepath = f'{input_folder}\\{input_filename}'
-	output_filepath = f'{output_folder}\\{output_filename}'
+	try:
+		print('Starting Parsing...')
+		start_time = datetime.now()
+		total_kanji = 0
+		current_execution_time = timedelta()
+		estimated_execution_time = datetime.min
+		estimated_execution_datetime = datetime.min
+		
+		input_filepath = f'{input_folder}\\{input_filename}'
+		output_filepath = f'{output_folder}\\{output_filename}'
 
 	os.makedirs(os.getcwd() + '\\' + input_folder, exist_ok=True)
 	os.makedirs(os.getcwd() + '\\' + output_folder, exist_ok=True)
 
-	csv_dict = get_data_from_kanji('新')
+		# Open the file initially to count the number of kanji that will be parsed
+		# This must be a separate open block from the one below, since the line parsing gets messed up otherwise 
+		with open(input_filepath, 'r', encoding='utf-8') as csv_input_file:
+			csv_reader = csv.reader(csv_input_file)
+			headers = next(csv_reader)
+			total_kanji = len(list(csv_reader))
+			print('Total Kanji To Parse: {}\n'.format(len(list(csv_reader))))
 
 	with open(input_filepath, 'r', encoding='utf-8') as csv_input_file:
 		csv_reader = csv.reader(csv_input_file)
