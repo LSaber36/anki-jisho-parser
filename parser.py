d67c51b7e4eb61d6c154107492f6a1bc3a121dd2
LSaber36 <L.Saber36@gmail.com>
2024-12-12 20:50:39 -0800

    Added progress bar and completion prediction

--- parser.py
+++ parser.py
@@ -87,15 +87,12 @@ def get_kanji_data(kanji, show_data=True, is_radical=False):
 
 	# Verify that the request was able to successfully get the website data
 	if html_content is not None:
-	soup = BeautifulSoup(html_content, 'html.parser')
+		soup = BeautifulSoup(html_content, 'html.parser')
+		
+		meaning_element = soup.find('div', {'class':'kanji-details__main-meanings'})
+		csv_dict[Headers.Meaning] = meaning_element.get_text().strip()
 
-
-	csv_dict[Headers.Front] = kanji
-	
-	meaning_element = soup.find('div', {'class':'kanji-details__main-meanings'})
-	csv_dict[Headers.Meaning] = meaning_element.get_text().strip()
-
-	yomi_elements = soup.find('div', {'class':'kanji-details__main-readings'})
+		yomi_elements = soup.find('div', {'class':'kanji-details__main-readings'})
 
 		kunyomi_elements = yomi_elements.find('dl', {'class':'kun_yomi'})
 		# This allows us to skip this step if there's no Kunyomi, which is common for radicals
@@ -121,7 +118,7 @@ def get_kanji_data(kanji, show_data=True, is_radical=False):
 			csv_dict[Headers.Example_Word] = example_data[Headers.Example_Word]
 			csv_dict[Headers.Example_Meaning] = example_data[Headers.Example_Meaning]
 
-		radical_dict = get_data_from_kanji(radical, is_radical=True)
+			radical_dict = get_kanji_data(radical, is_radical=True)
 
 			csv_dict[Headers.Radical_Meaning] = radical_dict[Headers.Meaning]
 			csv_dict[Headers.Radical_Onyomi] = radical_dict[Headers.Onyomi]
@@ -156,8 +153,8 @@ if __name__ == '__main__':
 		input_filepath = f'{input_folder}\\{input_filename}'
 		output_filepath = f'{output_folder}\\{output_filename}'
 
-	os.makedirs(os.getcwd() + '\\' + input_folder, exist_ok=True)
-	os.makedirs(os.getcwd() + '\\' + output_folder, exist_ok=True)
+		os.makedirs(os.getcwd() + '\\' + input_folder, exist_ok=True)
+		os.makedirs(os.getcwd() + '\\' + output_folder, exist_ok=True)
 
 		# Open the file initially to count the number of kanji that will be parsed
 		# This must be a separate open block from the one below, since the line parsing gets messed up otherwise 
@@ -167,13 +164,54 @@ if __name__ == '__main__':
 			total_kanji = len(list(csv_reader))
 			print('Total Kanji To Parse: {}\n'.format(len(list(csv_reader))))
 
-	with open(input_filepath, 'r', encoding='utf-8') as csv_input_file:
-		csv_reader = csv.reader(csv_input_file)
+		with open(input_filepath, 'r', encoding='utf-8') as csv_input_file:
+			csv_reader = csv.reader(csv_input_file)
 
-		with open(output_filepath, 'w', newline='', encoding='utf-8') as csv_output_file:
-			csv_writer = csv.writer(csv_output_file)
+			with open(output_filepath, 'w', newline='', encoding='utf-8') as csv_output_file:
+				csv_writer = csv.writer(csv_output_file)
 
-			# for row in csv_reader:
-			# 	print(row[0])
+				# Use this to skip the first line in the CSV
+				input_csv_headers = next(csv_reader)
 
-			output_to_csv(csv_writer, csv_dict)
+				# Write the output CSV headers
+				csv_writer.writerow(csv_dict_blank.keys())
+
+				progress_bar = ProgressBar(\
+					prefix='', suffix='', wrap_bar_prefix='[', wrap_bar_suffix='] ',\
+					complete_symbol='█', not_complete_symbol='-', progress_explain='',\
+					progress_loc=ProgressBar.PROGRESS_LOC_END, \
+					bar_length=30, total=total_kanji
+				)
+
+				for row_index, row in enumerate(csv_reader):
+					parse_time_start = datetime.now()
+					kanji = row[0]
+
+					# Write the new kanji data to the next row in the file
+					csv_writer.writerow(get_kanji_data(kanji).values())
+
+					parse_time_total = datetime.now() - parse_time_start
+					current_execution_time += parse_time_total
+
+					# Only after the first kanji parse, calculate the time to parse the rest
+					if row_index == 0:
+						estimated_execution_time = parse_time_total * total_kanji
+						estimated_execution_datetime = datetime.now() + estimated_execution_time - parse_time_total
+
+					remaining_execution_time = estimated_execution_time - current_execution_time
+
+					print()
+					print('This Parse Time:  {}'.format(str(parse_time_total).split('.')[0]))
+					print('Total Parse Time: {}'.format(str(current_execution_time).split('.')[0]))
+					print('Remaining Time:   {}'.format(str(remaining_execution_time).split('.')[0]))
+					print('Completion Time:  {}'.format(estimated_execution_datetime.strftime('%I:%M %p')))
+					progress_bar.set_stat(row_index+1)
+					progress_bar.update()
+					print()
+
+				progress_bar.end()
+
+		total_time = datetime.now() - start_time
+		print(f'Total Execution Time: {total_time}')
+	except Exception as e:
+		print('Encountered Exception In Main:\n{}'.format(traceback.print_exc(e)))
